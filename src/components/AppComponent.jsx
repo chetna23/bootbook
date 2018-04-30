63a7c935af3bb8bb804406a24143b217f3083110
caggarw <chetna.aggarwal@monsanto.com>
2018-04-30 16:11:39 -0500

    blah

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -3,20 +3,34 @@ import HeaderCardComponent from './HeaderCardComponent';
 import CreateStatusComponent from './CreateStatusComponent';
 import StatusesComponent from './StatusesComponent';
 
+let statusId = 0;
+
 class AppComponent extends React.Component {
     constructor() {
         super();
 
         this.state = {statuses: []};
         this.postStatus = this.postStatus.bind(this);
+        this.removeStatus = this.removeStatus.bind(this);
     }
 
     postStatus(statusText) {
         const {statuses} = this.state;
-        const newStatus = {id: statuses.length, text: statusText}
+        const newStatus = {id: statusId++, text: statusText}
         this.setState({statuses: statuses.concat(newStatus)});
     }
 
+    removeStatus(event) {
+        const {statuses} = this.state;
+        const statusToRemoveId = Number(event.currentTarget.dataset.statusId);
+        const updatedStatuses = statuses.filter(status => {
+            if (status.text === 'DO NOT REMOVE') {
+                return true;
+            }
+            return status.id !== statusToRemoveId;
+        });
+        this.setState({statuses: updatedStatuses});
+    }
     render() {
         const {statuses} = this.state;
 
@@ -27,7 +41,7 @@ class AppComponent extends React.Component {
 
                 <HeaderCardComponent name="Chetna Aggarwal" />
                 <CreateStatusComponent postStatus={this.postStatus} />
-                <StatusesComponent statuses={statuses} />
+                <StatusesComponent statuses={statuses} removeStatus={this.removeStatus} />
             </div>
         );
     }
--- src/components/StatusesComponent.jsx
+++ src/components/StatusesComponent.jsx
@@ -1,13 +1,26 @@
 import React        from 'react';
 import PropTypes    from 'prop-types';
 
-const StatusesComponent = ({statuses}) => (
+const StatusesComponent = ({statuses, removeStatus}) => (
     <div>
+        {statuses.length === 0 &&
+        <div className="well well-sm">
+            No Statuses to display
+        </div>
+        }
         {statuses.map(status => (
             <div
                 key={status.id} className="list-group-item"
             >
                 {status.text}
+                <button type="button"
+                    classID="close"
+                    aria-label="Remove status"
+                    data-status-id={status.id}
+                    onClick={removeStatus}
+                >
+                    <span className="fa fa-close" aria-hidden="true" />
+                </button>
             </div>
         ))}
     </div>
@@ -19,7 +32,8 @@ const statusShape = PropTypes.shape({
     id: PropTypes.number.isRequired, text: PropTypes.string.isRequired});
 
 StatusesComponent.propTypes = {
-    statuses: PropTypes.arrayOf(statusShape).isRequired
+    statuses: PropTypes.arrayOf(statusShape).isRequired,
+    removeStatus: PropTypes.func.isRequired
 };
 
 export default StatusesComponent;
